5dac86c3f3b309abab95c094724cfc6a81cf8cfe
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-29 21:02:00 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+#region IMPORTS
+
+
+#endregion
+
+
+#region INIT DATA
+company = {
+    'id': 1,
+    'name': 'ABC Software d.o.o.',
+    'tax_id': '01234567890',
+    'hq': {
+        'street': 'Duga ulica 15',
+        'postal_code': '10290',
+        'city': 'Zapresic',
+        'country': 'Hrvatska'
+    },
+    'email': 'info@abc-software.hr'
+}
+
+bank = {
+    'id': 1,
+    'name': 'Lipa po lipa d.d.',
+    'hq': {
+        'street': 'Kratka ulica 5',
+        'postal_code': '10290',
+        'city': 'Zapresic',
+        'country': 'Hrvatska'
+    }
+}
+
+currency = {
+    'id': 1,
+    'name': 'Euro',
+    'symbol': '€',
+    'code': 'EUR'
+}
+
+bank_account = {
+    'id': 1,
+    'IBAN': 'HR45875465481354654',
+    'balance': 0.00,
+    'opening_date': '2025-09-29',
+    'currency': currency
+}
+
+
+
+
+#endregion
