e7e0e96ff7166d953b159d6c25686bb89be9f160
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-01 20:22:33 +0200

    Update testiranje.py

--- testiranje.py
+++ testiranje.py
@@ -52,32 +52,44 @@ company = {
 
 #endregion
 
-
+'''
 # Prikaz detalja racuna
-def transform_key(key: str) -> str:
-    keys = key.split('_')
-    if len(keys) == 1:
-        return f'{keys[0].capitalize()}'
-    else:
-        title = ''
-        for element in keys:
-            title += f'{element.capitalize()} '
-        return title
+# def transform_key(key: str) -> str:
+#     keys = key.split('_')
+#     if len(keys) == 1:
+#         return f'{keys[0].capitalize()}'
+#     else:
+#         title = ''
+#         for element in keys:
+#             title += f'{element.capitalize()} '
+#         return title
 
 
-def display_entity_details(entity: Dict) -> None:
-    # print(f'{"ID":<15} {bank_account['id']:<20}')
-    # print(f'{"IBAN":<15} {bank_account['IBAN']:<20}')
-    for key, value in entity.items():
-        key = transform_key(key)
-        if type(value) == dict:
-            print()
-            print(key)
-            display_entity_details(value)
-        elif type(value) == float:
-            print(f'{key:<15} {value:>15.3f} EUR')
-        else:
-            print(f'{key:<15} {str(value):<20}')
+# def display_entity_details(entity: Dict) -> None:
+#     # print(f'{"ID":<15} {bank_account['id']:<20}')
+#     # print(f'{"IBAN":<15} {bank_account['IBAN']:<20}')
+#     for key, value in entity.items():
+#         key = transform_key(key)
+#         if type(value) == dict:
+#             print()
+#             print(key)
+#             display_entity_details(value)
+#         elif type(value) == float:
+#             print(f'{key:<15} {value:>15.3f} EUR')
+#         else:
+#             print(f'{key:<15} {str(value):<20}')
 
 
-display_entity_details(bank_account)
+# display_entity_details(bank_account)
+'''
+
+
+# Trebamo napraviti funkciju koja ispisuje elemente bilo kojeg rjecnika 
+# tako da ispise kljuc kojem poveca prvo slovo i nakon toga vrijednost uz taj kljuc.
+
+def print_dict(dictionary: Dict) -> None:
+    for key, value in dictionary.items():
+        print(key, value)
+
+
+print_dict(currency)
